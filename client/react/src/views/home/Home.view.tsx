d71f5e660971ef72a8aac23282841a4b33fdf307
longyiszh <longyiszh@outlook.com>
2022-05-31 19:34:08 -0400

    get user data with axios

--- client/react/src/views/home/Home.view.tsx
+++ client/react/src/views/home/Home.view.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { AddUser } from "src/components/addUser/AddUser";
 import { AuthInfo, IAuthInfoProps } from "src/components/authInfo/AuthInfo";
 import { UserList } from "src/components/userList/UserList";
-import { IInputUser } from "src/models/user.interface";
+import { IInputUser, IUser } from "src/models/user.interface";
 import styles from "./Home.view.module.css";
 
 export interface IHomeViewProps {
@@ -11,11 +12,35 @@ export interface IHomeViewProps {
 
 export const HomeView = (props: IHomeViewProps) => {
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([] as IUser[]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // load User records from server
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get<IUser[]>(`${process.env.REACT_APP_API_GET_USER_URL}`);
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 
   const onUserAdd = (newRecords: IInputUser[]) => {
     console.log(`User added`, newRecords);
   };
+
+  useEffect(() => {
+    if (!isLoaded) {
+      getUsers()
+        .then((data) => {
+          if (data) {
+            setUsers(data);
+          }
+          setIsLoaded(true);
+        });
+    }
+  });
 
   return (
     <section className={styles.HomeView}>
